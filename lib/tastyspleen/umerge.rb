57c4f3eca634e71abe9d24cb61535bd1a9ef35f7
spatulasnout <spatulasnout@tastyspleen.net>
2018-07-21 19:51:45 -0700

    Rewrite merge logic to handle a circularity the previous version couldn't resolve.

--- lib/tastyspleen/umerge.rb
+++ lib/tastyspleen/umerge.rb
@@ -41,6 +41,12 @@ module TastySpleen
       self
     end
     
+    def dup
+      copy = self.class.allocate
+      copy.instance_variable_set(:@data, @data.dup)
+      copy
+    end
+    
     def size
       @data.size
     end
@@ -53,6 +59,33 @@ module TastySpleen
       @data << row
     end
     
+    def sort_by_ids!
+      @data = @data.sort_by do |row|
+        id = row[IDX_UID].to_i
+        id = 99.9 if (id == 65534)  # hideous kludge so 'nobody/nogroup' stays just below 100 in the list (aesthetic reasons only)
+        id
+      end
+    end
+    
+    def ids_to_integer!(row_idx)
+      @data.each do |row|
+        id = row[row_idx]
+        Integer(id)  # sanity check (but it could convert to octal, so ignore result)
+        id = id.to_i
+        row[row_idx] = id
+      end
+    end
+    
+    def uids_to_integer!
+      ids_to_integer!(IDX_UID)
+    end
+    
+    alias gids_to_integer! uids_to_integer!
+    
+    def ugids_to_integer!
+      ids_to_integer!(IDX_UGID)
+    end
+    
     def find_by_name(name)
       @data.find {|row| row[IDX_NAME] == name}
     end
@@ -60,23 +93,23 @@ module TastySpleen
     alias find_by_uname find_by_name
     alias find_by_gname find_by_name
     
-    def find_by_uid(uid)
-      uid = Integer(uid)
-      @data.find {|row| row[IDX_UID].to_i == uid}
+    def find_by_id(id)
+      @data.find {|row| (! (rid = row[IDX_UID]).kind_of?(Array)) && (rid == id)}
     end
 
+    alias find_by_uid find_by_id
+    alias find_by_gid find_by_id
+    
     def find_available_uids(range_low, range_high)
       uids = (range_low..range_high).to_a
-      self.each {|row| uids.delete(row[IDX_UID].to_i) if row[IDX_UID]}
+      self.each {|row| uids.delete(row[IDX_UID]) if row[IDX_UID]}
       uids
     end
     
     def build_name_to_id_map
       name_to_id = {}
       @data.each do |row|
-        id = Integer(row[IDX_UID])
-        name = row[IDX_NAME]
-# (id.to_i == 110) and warn("\nbuild_name_to_id_map: 110 here: #{row.inspect}")
+        name, id = row[IDX_NAME], row[IDX_UID]
         (name_to_id.key?(name)) and raise("duplicate mapping for name #{name.inspect}")
         name_to_id[name] = id
       end      
@@ -89,8 +122,7 @@ module TastySpleen
     def build_gid_to_gname_map
       gid_to_gname = {}
       @data.each do |row|
-        gid = Integer(row[IDX_GID])
-        gname = row[IDX_NAME]
+        gname, gid = row[IDX_NAME], row[IDX_GID]
         (gid_to_gname.key?(gid)) and raise("duplicate mapping for gid #{gid.inspect}")
         gid_to_gname[gid] = gname
       end
@@ -100,8 +132,7 @@ module TastySpleen
     def build_gname_to_gid_map
       gname_to_gid = {}
       @data.each do |row|
-        gid = Integer(row[IDX_GID])
-        gname = row[IDX_NAME]
+        gname, gid = row[IDX_NAME], row[IDX_GID]
         (gname_to_gid.key?(gname)) and raise("duplicate mapping for gname #{gname.inspect}")
         gname_to_gid[gname] = gid
       end
@@ -128,6 +159,9 @@ module TastySpleen
       @shadow  = UData.new.load(File.join(path, SHADOW_FNAME))
       @group   = UData.new.load(File.join(path, GROUP_FNAME))
       @gshadow = UData.new.load(File.join(path, GSHADOW_FNAME))
+      @passwd.uids_to_integer!
+      @passwd.ugids_to_integer!
+      @group.gids_to_integer!
       self
     end
     
@@ -137,6 +171,11 @@ module TastySpleen
       @group.save(File.join(path, GROUP_FNAME))
       @gshadow.save(File.join(path, GSHADOW_FNAME))
       self
+    end
+    
+    def sort_by_ids!
+      @passwd.sort_by_ids!
+      @group.sort_by_ids!
     end
   end # UMerge::DataSet
   
@@ -166,49 +205,147 @@ module TastySpleen
       src  = DataSet.new.load(src_dir)
       remap_ugid_to_symbolic(dest.passwd, dest.group)
       remap_ugid_to_symbolic(src.passwd, src.group)
-      preferred_name_to_uid = (preferred_name_to_uid_generator) ? preferred_name_to_uid_generator.call(src.passwd) : {}
-      merge_udata(st, dest, src, preferred_name_to_uid)
+# warn "--- merge_udata --------------------------------------------------------"
+      merge_udata(st, dest, src, preferred_name_to_uid_generator)
+# warn "--- merge_gdata --------------------------------------------------------"
       merge_gdata(st, dest, src)
       remap_ugid_to_numeric(src.passwd, src.group)
       remap_ugid_to_numeric(dest.passwd, dest.group)
+      dest.sort_by_ids!
+      src.sort_by_ids!
       dest.save(dest_output_dir)
       src.save(src_output_dir)
       [st.uremaps, st.gremaps]
     end
     
-    def merge_udata(st, dest, src, preferred_name_to_uid={})
-# warn "\nmerge_udata: preferred_name_to_uid=#{preferred_name_to_uid.inspect}"
-      merge_data(st, dest.passwd, dest.shadow, src.passwd, src.shadow, st.uremaps, ?u, preferred_name_to_uid)
+    protected
+
+    def gen_required_name_to_id(dest_rows, preferred_name_to_id, mtype, warn_no_preferred=true)
+      required_name_to_id = dest_rows.build_name_to_id_map
+      
+      uniq_ids = {}
+      required_name_to_id.each_pair do |name, id|
+        (uniq_ids.key?(id)) and raise("ERROR: duplicate #{mtype}id #{id}")
+        uniq_ids[id] = name
+      end
+      
+      preferred_name_to_id.each_pair do |name, id|
+        if required_name_to_id.key?(name)
+          if (d_id = required_name_to_id[name]) != id
+            warn("WARN: preferred #{mtype}id #{name}=>#{id} collides with dest #{name}=>#{d_id}, ignoring") if warn_no_preferred
+          end
+        else
+          if uniq_ids.key?(id)
+            warn("WARN: preferred #{mtype}id #{name}=>#{id} collides with dest #{uniq_ids[id]}=>#{id}, ignoring") if warn_no_preferred       
+          else
+            required_name_to_id[name] = id
+            uniq_ids[id] = name
+          end
+        end
+      end
+      required_name_to_id
+    end
+
+    def merge_udata(st, dest, src, preferred_name_to_uid_generator)
+      preferred_name_to_id = (preferred_name_to_uid_generator) ? preferred_name_to_uid_generator.call(src.passwd) : {}
+      required_name_to_id = gen_required_name_to_id(dest.passwd, preferred_name_to_id, ?u)
+
+# warn "\nmerge_udata: required_name_to_id=#{required_name_to_id.inspect}"
+
+      merge_data(st, dest.passwd, dest.shadow, src.passwd, src.shadow, st.uremaps, ?u, required_name_to_id)
     end
     
     def merge_gdata(st, dest, src)
       preferred_name_to_id = dest.passwd.build_name_to_uid_map  # try to keep gids with uids
-# warn "\nmerge_gdata: preferred_name_to_id=#{preferred_name_to_id.inspect}"
-      merge_data(st, dest.group, dest.gshadow, src.group, src.gshadow, st.gremaps, ?g, preferred_name_to_id)
+      
+      warn_no_preferred = false  # non-preferred mappings are common here, as we're just trying to map gids to corresponding uids if possible
+      required_name_to_id = gen_required_name_to_id(dest.group, preferred_name_to_id, ?g, warn_no_preferred)
+    
+# warn "\nmerge_gdata: required_name_to_id=#{required_name_to_id.inspect}"
+
+      merge_data(st, dest.group, dest.gshadow, src.group, src.gshadow, st.gremaps, ?g, required_name_to_id)
     end
 
-    def merge_data(st, dest_passwd, dest_shadow, src_passwd, src_shadow, remaps, mtype, preferred_name_to_id={})
-      pass = 0
-      begin
-        pass += 1
-        st.n_remaps = 0
-        st.collisions.clear
-# warn "\npass... dest_sz=#{dest_passwd.size} src_sz=#{src_passwd.size}"
-        merge_data_pass(st, dest_passwd, dest_shadow, src_passwd, src_shadow, remaps, mtype, preferred_name_to_id, pass)
-        done = (st.n_remaps.zero? && st.collisions.empty?) || (pass > (src_passwd.size * 2))
-      end until done
+    SYSTEM_IDS_BANK_SIZE = 1000
+
+    def merge_data(st, dest_db, dest_shadow, src_db, src_shadow, remaps, mtype, required_name_to_id)
+      (required_name_to_id.values.size == required_name_to_id.values.uniq.size) or raise("ERROR: multiple names map to same value in required_name_to_id: #{required_name_to_id.inspect}")
       
-      unless st.collisions.empty?
-        raise "unresolvable collisions: #{st.collisions.inspect}"
+      # Pass 1.
+      #   Just remap all required src ids to required dest ids, if any.
+      #   
+      #   Any remaps that collide with existing ids in src, uh, get limboed
+      #
+      src_db.each do |row|
+# warn  "row=#{row.inspect}"
+        name, id = row[IDX_NAME], row[IDX_UID]
+        
+        next if (id.kind_of?(Numeric) && id.zero?)  # we don't mess with root
+        
+        # NOTE: <id> might be Array[id], if we limboed this row below
+        
+        if (r_id = required_name_to_id[name])
+# warn "pass_1: required: #{id.inspect}=>#{r_id.inspect} #{row.inspect}"
+          if id == r_id
+            copy(row, src_shadow, dest_db, dest_shadow)  # checks if already in dest
+          else
+            # Limbo any possible row with our new id in <src_db>
+            if (coll_row = src_db.find_by_id(r_id))
+              coll_row[IDX_UID] = [coll_row[IDX_UID]]
+# warn "LIMBOED: (#{mtype}ids) id=#{id} r_id=#{r_id} name=#{name} #{coll_row.inspect}"
+            end
+            
+            orig_id = (id.kind_of?(Array)) ? id.first : id
+            
+            remap_and_copy(st, row, orig_id, r_id, src_db, dest_db, dest_shadow, remaps)
+          end
+        end
+      end
+      
+      # Pass 2.
+      #   Find homes for any rows with non-required ids.
+      #
+      src_db.each do |row|
+        name, id = row[IDX_NAME], row[IDX_UID]
+        
+        next if ((id.kind_of?(Numeric) && id.zero?) || required_name_to_id[name])  # handled in pass 1
+        
+        (dest_db.find_by_name(name)) and raise("ERROR: unexpected: pass 2: name #{name.inspect} should not exist in dest, pass 1 should have handled all preexisting named rows (row=#{row.inspect})")
+        
+        # NOTE: <id> might be Array[id], if limboed above
+        
+        need_remap = (id.kind_of?(Array) || dest_db.find_by_id(id))
+# warn "pass_2: need_remap=#{need_remap.inspect} #{row.inspect}"
+        if need_remap
+          id = id.first if id.kind_of?(Array)
+          
+          bsize = SYSTEM_IDS_BANK_SIZE
+          bank = (id / bsize)
+          id_range_low = bank * bsize
+          id_range_high = (id_range_low + (bsize - 1))
+          if bank.zero?
+            # If we're in the system accounts bank, don't go lower
+            # than original <id>.  (This is arbitrary, but it feels
+            # weird to have a 103 map down to a 35, or whatnot.)
+            id_range_low = id
+          end
+          new_id = find_mutually_available_id(src_db, dest_db, id_range_low, id_range_high)
+          if new_id
+            remap_and_copy(st, row, id, new_id, src_db, dest_db, dest_shadow, remaps)
+          else
+            raise "out of #{mtype}ids remapping #{id}"
+          end
+          
+        else
+          copy(row, src_shadow, dest_db, dest_shadow)
+        end
       end
     end
-    
-    protected
     
     def remap_ugid_to_symbolic(passwd, group)
       gid_to_gname = group.build_gid_to_gname_map
       passwd.each do |row|
-        ugid = Integer(row[IDX_UGID])
+        ugid = row[IDX_UGID]
         gname = gid_to_gname[ugid]
         (ugid && gname) or raise("missing gid_to_gname map for ugid=#{ugid.inspect} gname=#{gname.inspect}")
         row[IDX_UGID] = gname
@@ -220,112 +357,38 @@ module TastySpleen
       passwd.each do |row|
         gname = row[IDX_UGID]  # changed from ugid to gname by prior remap
         ugid = gname_to_gid[gname]
-        (gname && ugid) or raise("missing gname_to_gid map for gname=#{gname.inspect} ugid=#{ugid.inspect}")
+        (gname && ugid) or raise("missing gname_to_gid map for gname=#{gname.inspect} ugid=#{ugid.inspect} gname_to_gid=#{gname_to_gid.inspect}")
         row[IDX_UGID] = ugid
       end
     end
+
+    def remap_and_copy(st, row, orig_id, new_id, src_shadow, dest_db, dest_shadow, remaps)
+      name = row[IDX_NAME]
+# warn "remap #{name} #{orig_id}=>#{new_id}"
+      row[IDX_UID] = new_id
+      remaps << [name, orig_id, new_id]
+      st.n_remaps += 1
+      copy(row, src_shadow, dest_db, dest_shadow)
+    end
     
-    # FIRST_USER_UID = 1000
-    
-    def merge_data_pass(st, dest_passwd, dest_shadow, src_passwd, src_shadow, remaps, mtype, preferred_name_to_id, pass)
-      src_passwd.each do |row|
-# warn  "row=#{row.inspect}"
-        uname, uid = row[IDX_NAME], row[IDX_UID].to_i
-        
-        dest_row = dest_passwd.find_by_uname(uname)
-        if dest_row
-          dest_uid = dest_row[IDX_UID].to_i
-          if uid == dest_uid
-            # Already exists in dest with same uid, nothing to do.
-          else
-            # Name exists in dest, but uid mismatch.
-            # If we remap, would there be a collision in *SRC*?
-            coll_src_row = src_passwd.find_by_uid(dest_uid)
-            if coll_src_row
-              # If we tried to remap uid to dest_uid, we'd collide
-              # with another uid already in src.
-              coll_uname, coll_uid = coll_src_row[IDX_NAME], coll_src_row[IDX_UID].to_i
-              st.collisions << "src(#{uname}:#{uid}=>#{coll_uname}:#{coll_uid})"
-            else
-              # Safe to remap.
-              row[IDX_UID] = dest_uid
-              remaps << [uname, uid, dest_uid]
-              st.n_remaps += 1
-            end
-          end
-        else
-          # Name does not exist in dest yet.
-          # Could we copy directly? Or is remap still needed to 
-          # avoid collision?
-          #
-          # For first round of passes, report src-collision if we're blocked
-          # trying to remap to preferred id. (First round of N passes could
-          # shuffle ids out of the way.)
-          coll_src_row = nil
-          remap_to_preferred = (preferred_uid = preferred_name_to_id[uname]) && (uid != preferred_uid) &&
-                                (! dest_passwd.find_by_uid(preferred_uid)) &&
-                                ((! (coll_src_row = src_passwd.find_by_uid(preferred_uid))) || (pass <= src_passwd.size))
-          
-          if remap_to_preferred
-            if coll_src_row
-              # We'll get here if we're in the first round of N(=src.size) passes
-              # and we hit a src=>src conflict trying to remap to preferred id.
-              # (After that, we'll have given up on preferred id collision 
-              # resolution.)
-              coll_uname, coll_uid = coll_src_row[IDX_NAME], coll_src_row[IDX_UID].to_i
-              st.collisions << "src(#{uname}:#{uid}=>#{coll_uname}:#{coll_uid})"
-            else
-              remap_and_copy(st, row, uname, uid, preferred_uid, src_shadow, dest_passwd, dest_shadow, remaps)
-            end
-          else
-            (preferred_uid && (uid != preferred_uid)) && warn("WARN: pass #{pass} failed to remap #{uname.inspect} to preferred_#{mtype}id #{preferred_uid.inspect}")
-            
-            coll_dest_row = dest_passwd.find_by_uid(uid)
-            if coll_dest_row
-              # Src uid already exists in dest under a different name.
-              # We need to remap.
-              # is_system_uid = (uid < FIRST_USER_UID)
-              bank = (uid / 1000)
-              uid_range_low = bank * 1000
-              uid_range_high = (uid_range_low + 999)
-              if bank.zero?
-                # If we're in the system accounts bank, don't go lower
-                # than original <uid>.  (This is arbitrary, but it feels
-                # weird to have a 103 map down to a 35, or whatnot.)
-                uid_range_low = uid
-              end
-              new_uid = find_mutually_available_uid(src_passwd, dest_passwd, uid_range_low, uid_range_high)
-              if new_uid
-                remap_and_copy(st, row, uname, uid, new_uid, src_shadow, dest_passwd, dest_shadow, remaps)
-              else
-                raise "out of #{mtype}ids remapping #{uid}"
-              end
-            else
-              # No conflict. We can just copy.
-              dest_passwd << row.dup
-              shadow_row = src_shadow.find_by_uname(uname)
-              if shadow_row
-                dest_shadow << shadow_row.dup
-              end
-              st.n_remaps += 1  # not really a "remap", just an 'action'
-            end
-          end
+    def copy(row, src_shadow, dest_db, dest_shadow)
+      name = row[IDX_NAME]
+      if (d_row = dest_db.find_by_name(name))
+        (d_row[IDX_UID] == row[IDX_UID]) or raise("ERROR: unexpected: adding #{name}=>#{row[IDX_UID]} to dest, but name with mismatching id already exists (row=#{row.inspect}, d_row=#{d_row.inspect})")
+        # already in dest, nothing to copy
+# warn "nocp: #{row.inspect}"
+      else
+        (! dest_db.find_by_id(row[IDX_UID])) or raise("ERROR: unexpected: adding #{name}=>#{row[IDX_UID]} to dest, but id already exists (row=#{row.inspect})")
+# warn "copy: #{row.inspect}"
+        dest_db << row.dup
+        shadow_row = src_shadow.find_by_name(name)
+        if shadow_row
+          dest_shadow << shadow_row.dup
         end
       end
     end
     
-    def remap_and_copy(st, row, uname, orig_uid, new_uid, src_shadow, dest_passwd, dest_shadow, remaps)
-      row[IDX_UID] = new_uid
-      remaps << [uname, orig_uid, new_uid]
-      st.n_remaps += 1
-      dest_passwd << row.dup
-      shadow_row = src_shadow.find_by_uname(uname)
-      if shadow_row
-        dest_shadow << shadow_row.dup
-      end
-    end
-    
-    def find_mutually_available_uid(p1, p2, uid_range_low, uid_range_high)
+    def find_mutually_available_id(p1, p2, uid_range_low, uid_range_high)
       uids1 = p1.find_available_uids(uid_range_low, uid_range_high)
       uids2 = p2.find_available_uids(uid_range_low, uid_range_high)
       uids = (uids1 & uids2)
